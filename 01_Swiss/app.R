f43a8b245fe8a8faa04ca76c844574ed59218202
Sandra Schneider <se17m030@technikum-wien.at>
2018-05-30 13:42:26 +0200

    Add boxplot and location measures

--- 01_Swiss/app.R
+++ 01_Swiss/app.R
@@ -11,7 +11,6 @@ ui <- fluidPage(
       selectInput("outcome", label = h3("Select variable:"),
                   choices = list("Fertility" = "Fertility",
                                  "Agriculture" = "Agriculture",
-                                 "Examination" = "Examination",
                                  "Education" = "Education",
                                  "Catholic" = "Catholic",
                                  "Infant.Mortality" = "Infant.Mortality"), selected = 1),
@@ -19,10 +18,15 @@ ui <- fluidPage(
       selectInput("indepvar", label = h3("Select second variable"),
                   choices = list("Fertility" = "Fertility",
                                  "Agriculture" = "Agriculture",
-                                 "Examination" = "Examination",
                                  "Education" = "Education",
                                  "Catholic" = "Catholic",
-                                 "Infant.Mortality" = "Infant.Mortality"), selected = 1)
+                                 "Infant.Mortality" = "Infant.Mortality"), selected = 1),
+      
+      # TODO: Show this selection in plots
+      selectInput("location", label = h3("Select location"),
+                  choices = list("Mean" = "Mean",
+                                 "Median" = "Median",
+                                 "Mode" = "Mode"), selected = 1)
       
     ),
     
@@ -30,8 +34,9 @@ ui <- fluidPage(
       
       tabsetPanel(type = "tabs",
                   
-                  tabPanel("Plot", plotOutput("plot")), # Plot
-                  tabPanel("Distribution", plotOutput("distribution")),
+                  tabPanel("Plot", plotOutput("plot")),
+                  # TODO: integrate Distribution into plot?
+                  tabPanel("Distribution", plotOutput("distribution"), plotOutput("boxplot")),
                   tabPanel("Summary", verbatimTextOutput("summary")),
                   tabPanel("Data", DT::dataTableOutput('tbl')) # Data as datatable
                   
@@ -55,7 +60,6 @@ server <- function(input, output) {
     DT::datatable(swiss, options = list(lengthChange = FALSE))
   })
   
-  
   # Plot output
   output$plot <- renderPlot({
     # TODO: Check if scatterplot makes sense -> variables are different
@@ -66,15 +70,45 @@ server <- function(input, output) {
       lines(lowess(swiss[,input$indepvar],swiss[,input$outcome]), col="blue")
     }
     else if (input$plot == "bar") {
-      barplot(swiss[,input$outcome])
+      barplot(swiss[,input$outcome], xlab=input$outcome)
     }
   })
   
+  # Plot boxplot
+  output$boxplot <- renderPlot({
+    # without frame: frame = F
+    # without axes: axes = FALSE
+    # TODO: Align boxplot with histogram axis
+    boxplot(swiss[,input$outcome], horizontal = TRUE, staplewex = 1)
+  })
   
   # Histogram output for distribution
   output$distribution <- renderPlot({
-    hist(swiss[,input$outcome], main="", xlab=input$outcome)
-  }, height=300, width=300)
+    h <- hist(swiss[,input$outcome], main="", xlab=input$outcome)
+    
+    # set density line
+    xfit<-seq(min(swiss[,input$outcome]),max(swiss[,input$outcome]),length=40) 
+    yfit<-dnorm(xfit,mean=mean(swiss[,input$outcome]),sd=sd(swiss[,input$outcome])) 
+    yfit <- yfit*diff(h$mids[1:2])*length(swiss[,input$outcome]) 
+    lines(xfit, yfit, col="blue", lwd=2)
+    
+    # TODO: show location based on selected location value?
+    abline(v = median(swiss[,input$outcome]),
+           col = "purple",
+           lwd = 2)
+    abline(v = mean(swiss[,input$outcome]),
+           col = "red",
+           lwd = 2)
+    abline(v = mode(swiss[,input$outcome]),
+           col = "green",
+           lwd = 2)
+    
+    # show legend
+    legend(x = "topright",
+           c("Density plot", "Mean", "Median", "Mode"),
+           col = c("blue", "purple", "red", "green"),
+           lwd = c(2, 2, 2))
+  })
 }
 
 shinyApp(ui = ui, server = server)
