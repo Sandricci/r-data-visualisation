825642eb016020bb9abfc3515af42ed0bd3577ce
Roman Bratschun <rbratschun@gmail.com>
2018-06-09 20:05:29 +0200

    add tab "correlations" with scatterplot matrix

--- 01_Swiss/app.R
+++ 01_Swiss/app.R
@@ -37,6 +37,7 @@ ui <- fluidPage(
                            htmlOutput("measures"), htmlOutput("locations"),htmlOutput("variations"), 
                            renderPlot("lm")),
                   tabPanel("Distribution", plotOutput("distribution"), plotOutput("boxplot")),
+                  tabPanel("Correlations", plotOutput("correlations")),
                   tabPanel("Summary", verbatimTextOutput("summary")),
                   tabPanel("Data", DT::dataTableOutput('tbl')) # Data as datatable
                   
@@ -168,6 +169,24 @@ server <- function(input, output) {
     )
   )
   })
+  
+  # Plot Scatterplot Matrix
+  output$correlations <- renderPlot({
+    ## put (absolute) correlations on the upper panels,
+    ## with size proportional to the correlations.
+    panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+    {
+      usr <- par("usr"); on.exit(par(usr))
+      par(usr = c(0, 1, 0, 1))
+      r <- abs(cor(x, y))
+      txt <- format(c(r, 0.123456789), digits = digits)[1]
+      txt <- paste0(prefix, txt)
+      if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+      text(0.5, 0.5, txt, cex = cex.cor * r)
+    }
+    pairs(swiss, lower.panel = panel.smooth, upper.panel = panel.cor, main="Scatterplot Matrix")
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
