3f1d502f98e9802a1181bfa10d5d07c9c4c8b894
Roman Bratschun <rbratschun@gmail.com>
2018-06-10 16:52:53 +0200

    add regression analysis and basic linear model quality summary to exercise 3

--- 03_LakeHuron/app.R
+++ 03_LakeHuron/app.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(DT)
-
+library(moments)
 dat <- as.data.frame(LakeHuron)
 head(dat)
 
@@ -15,6 +15,8 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("plot", label = h3("Select visualisation:"),
                   choices = list("Bar" = "bar", "Scatterplot" = "scatter")),
+      selectInput("lags", label = h3("Number of lags:"),
+                  choices = c(1:24), selected = 1),
       
       checkboxGroupInput("time", label = h3("Select time"), 
                          choices = list("Time" = "time"),selected = 1),
@@ -29,9 +31,10 @@ ui <- fluidPage(
       
       tabsetPanel(type = "tabs",
                   
-                  tabPanel("Plot", plotOutput("plot"), verbatimTextOutput("correlation")),
+                  tabPanel("Plot", plotOutput("plot", height = 800), verbatimTextOutput("correlation")),
                   tabPanel("Distribution", plotOutput("distribution"), plotOutput("boxplot")),
-                  tabPanel("Summary", verbatimTextOutput("summary")),
+                  tabPanel("Regression Analysis", plotOutput("lmplot", height = 800, width = 1000)),
+                  tabPanel("Summary", verbatimTextOutput("summary"), verbatimTextOutput("corsummary")),
                   tabPanel("Data", DT::dataTableOutput('tbl')) # Data as datatable
                   
       )
@@ -43,30 +46,21 @@ ui <- fluidPage(
 # SERVER
 server <- function(input, output) {
   
-  # Regression output
-  output$summary <- renderPrint({
-    fit <- dat[,"Level"]
-    summary(fit)
-  })
-  
-  # Data output
-  output$tbl = DT::renderDataTable({
-    DT::datatable(dat, options = list(lengthChange = FALSE))
-  })
-  
   # Plot output
   output$plot <- renderPlot({
       if(!is.null(input$time)){
-        plot(LakeHuron)
+        plot(LakeHuron, ylab="depth (in feet)", xlab = "Time (in years)")
       }
       else {
-        lag.plot(LakeHuron,do.lines=F)
+        lag.plot(LakeHuron,as.numeric(input$lags),do.lines=F)
       }
   })
   
   # TODO
-  output$lm <- renderPlot({
-    simple.fit <- lm(Education~Fertility, data = dat)
+  output$correlation <- renderText({
+    if(input$plot == "scatter") {
+      cor(dat[,"Level"], dat[,"Year"], method = c("pearson"));
+    }
   })
   
   # TODO
@@ -84,20 +78,20 @@ server <- function(input, output) {
     h <- hist(dat[,"Level"], main="", xlab=dat[,"Year"])
     
     # set density line
-    xfit<-seq(min(dat[,input$outcome]),max(dat[,input$outcome]),length=40) 
-    yfit<-dnorm(xfit,mean=mean(dat[,input$outcome]),sd=sd(dat[,input$outcome])) 
-    yfit <- yfit*diff(h$mids[1:2])*length(dat[,input$outcome]) 
+    xfit<-seq(min(dat[,"Level"]),max(dat[,"Level"]),length=40) 
+    yfit<-dnorm(xfit,mean=mean(dat[,"Level"]),sd=sd(dat[,"Level"])) 
+    yfit <- yfit*diff(h$mids[1:2])*length(dat[,"Level"]) 
     lines(xfit, yfit, col="blue", lwd=2)
     
     for(i in input$location){
         switch(i, 
                "Mean"={
-                 abline(v = mean(dat[,input$outcome]),
+                 abline(v = mean(dat[,"Level"]),
                         col = "green",
                         lwd = 2)
                },
                "Median"={
-                 abline(v = median(dat[,input$outcome]),
+                 abline(v = median(dat[,"Level"]),
                         col = "red",
                         lwd = 2)   
                }
@@ -111,12 +105,32 @@ server <- function(input, output) {
            lwd = c(2, 2, 2))
   })
   
-  # TODO
-  output$correlation <- renderText({
-    if(input$plot == "scatter") {
-      cor(dat[,input$outcome], dat[,input$indepvar], method = c("pearson"));
-    }
-  }) 
+  output$lmplot <- renderPlot({
+    if(length(input$indepvar) == 0) return;
+    lm <- lm(dat[,"Level"] ~ dat[, "Year"])
+    par(mfrow=c(2,2))
+    plot(lm, which=1)
+    plot(lm, which=2)
+    plot(lm, which=3)
+    plot(lm, which=4)
+  })
+  
+  # 5 Number summary
+  output$summary <- renderPrint({
+    fit <- dat[,"Level"]
+    summary(fit)
+  })
+  
+  output$corsummary <- renderPrint({
+    # TODO - HOW TO generate a Model vs. Time Series
+    lm <- lm(dat[,"Level"] ~ dat[, "Year"])
+    summary(lm)
+  })
+  
+  # Data output
+  output$tbl = DT::renderDataTable({
+    DT::datatable(dat, options = list(lengthChange = FALSE))
+  })
 }
 
 shinyApp(ui = ui, server = server)
