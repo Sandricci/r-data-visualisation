906233c6376dfcb5965c88c5338b471e9434c3dd
Roman Bratschun <rbratschun@gmail.com>
2018-06-16 14:28:44 +0200

    finished Example 03 (Lake); add plots, moments, location and variation estimators

--- 03_LakeHuron/app.R
+++ 03_LakeHuron/app.R
@@ -1,29 +1,20 @@
 library(shiny)
 library(DT)
 library(moments)
-dat <- as.data.frame(LakeHuron)
-head(dat)
-
-# rename var x in dataset
-names(dat)[names(dat)=="x"] <- "Level"
-# set years per level
-dat["Year"] <- c(1875:1972)
 
 ui <- fluidPage(
   titlePanel("Exercise 3 - Dataset: LakeHuron"),
   sidebarLayout(
     sidebarPanel(
       selectInput("plot", label = h3("Select visualisation:"),
-                  choices = list("Bar" = "bar", "Scatterplot" = "scatter")),
-      selectInput("lags", label = h3("Number of lags:"),
-                  choices = c(1:24), selected = 1),
+                  choices = list("Plot" = "plot", "Time Series" = "series", "ACF" = "acf"), selected = "plot"),
       
-      checkboxGroupInput("time", label = h3("Select time"), 
-                         choices = list("Time" = "time"),selected = 1),
+      selectInput("lags", label = h3("Number of lags:"),
+                  choices = c(1:9), selected = 1),
       
       checkboxGroupInput("location", label = h3("Select location"), 
                          choices = list("Mean" = "Mean",
-                                        "Median" = "Median"), selected = 1)
+                                        "Median" = "Median"))
       
     ),
     
@@ -32,15 +23,16 @@ ui <- fluidPage(
       tabsetPanel(type = "tabs",
                   
                   tabPanel("Plot", plotOutput("plot", height = 800), verbatimTextOutput("correlation")),
+                  tabPanel("Momente, Lage & Variation", 
+                           column(htmlOutput("measures"), htmlOutput("locations"),htmlOutput("variations"), width = 6)),
                   tabPanel("Distribution", plotOutput("distribution"), plotOutput("boxplot")),
                   tabPanel("Regression Analysis", plotOutput("lmplot", height = 800, width = 1000)),
                   tabPanel("Summary", verbatimTextOutput("summary"), verbatimTextOutput("corsummary")),
-                  tabPanel("Data", DT::dataTableOutput('tbl')) # Data as datatable
+                  tabPanel("Data", fluidRow(column(DT::dataTableOutput('tbl'), width = 6))) # Data as datatable
                   
       )
     )
   ))
-
 
 
 # SERVER
@@ -48,50 +40,45 @@ server <- function(input, output) {
   
   # Plot output
   output$plot <- renderPlot({
-      if(!is.null(input$time)){
-        plot(LakeHuron, ylab="depth (in feet)", xlab = "Time (in years)")
-      }
-      else {
-        lag.plot(LakeHuron,as.numeric(input$lags),do.lines=F)
-      }
+    if(input$plot == "series") {
+      lag.plot(LakeHuron,as.numeric(input$lags),do.lines=F)
+    }
+    else if(input$plot == "plot") {
+      plot(LakeHuron, ylab="depth (in feet)", xlab = "Time (in years)") 
+    }
+    else if(input$plot == "acf") {
+      acf(LakeHuron, main ="Estimation of Auto-Correlation Function (ACF)", legend) 
+    } 
+    
   })
   
   # TODO
   output$correlation <- renderText({
-    if(input$plot == "scatter") {
-      cor(dat[,"Level"], dat[,"Year"], method = c("pearson"));
-    }
+    cor(LakeHuron, time(LakeHuron), method = c("spearman"));
   })
   
-  # TODO
-  # Plot boxplot
   output$boxplot <- renderPlot({
-    # without frame: frame = F
-    # without axes: axes = FALSE
-    # TODO: Align boxplot with histogram axis
-    boxplot(dat[,"Level"], horizontal = TRUE, staplewex = 1)
+    boxplot(LakeHuron, horizontal = TRUE, staplewex = 1)
   })
   
-  # TODO
-  # Histogram output for distribution
   output$distribution <- renderPlot({
-    h <- hist(dat[,"Level"], main="", xlab=dat[,"Year"])
+    h <- hist(LakeHuron, main="", xlab= time(LakeHuron))
     
     # set density line
-    xfit<-seq(min(dat[,"Level"]),max(dat[,"Level"]),length=40) 
-    yfit<-dnorm(xfit,mean=mean(dat[,"Level"]),sd=sd(dat[,"Level"])) 
-    yfit <- yfit*diff(h$mids[1:2])*length(dat[,"Level"]) 
+    xfit<-seq(min(LakeHuron),max(LakeHuron),length=40) 
+    yfit<-dnorm(xfit,mean=mean(LakeHuron),sd=sd(LakeHuron)) 
+    yfit <- yfit*diff(h$mids[1:2])*length(LakeHuron) 
     lines(xfit, yfit, col="blue", lwd=2)
     
     for(i in input$location){
         switch(i, 
                "Mean"={
-                 abline(v = mean(dat[,"Level"]),
+                 abline(v = mean(LakeHuron),
                         col = "green",
                         lwd = 2)
                },
                "Median"={
-                 abline(v = median(dat[,"Level"]),
+                 abline(v = median(LakeHuron),
                         col = "red",
                         lwd = 2)   
                }
@@ -106,30 +93,65 @@ server <- function(input, output) {
   })
   
   output$lmplot <- renderPlot({
-    if(length(input$indepvar) == 0) return;
-    lm <- lm(dat[,"Level"] ~ dat[, "Year"])
     par(mfrow=c(2,2))
-    plot(lm, which=1)
-    plot(lm, which=2)
-    plot(lm, which=3)
-    plot(lm, which=4)
+    fit <- lm(LakeHuron ~ time(LakeHuron))
+    plot(fit)
   })
   
   # 5 Number summary
   output$summary <- renderPrint({
-    fit <- dat[,"Level"]
-    summary(fit)
+    summary(LakeHuron)
   })
   
   output$corsummary <- renderPrint({
-    # TODO - HOW TO generate a Model vs. Time Series
-    lm <- lm(dat[,"Level"] ~ dat[, "Year"])
-    summary(lm)
+    fit <- lm(LakeHuron ~ time(LakeHuron))
+    summary(fit)
   })
   
   # Data output
   output$tbl = DT::renderDataTable({
-    DT::datatable(dat, options = list(lengthChange = FALSE))
+    t <- cbind(LakeHuron, time(LakeHuron))
+    colnames(t) <- c("Level", "Year")
+    DT::datatable(as.data.frame(t), options = list(lengthChange = FALSE, paging = TRUE, info = FALSE))
+  })
+  
+  output$measures <- renderUI({tagList(
+    tags$h4("Measures"),
+    tags$table(class="table table-condensed table-bordered table-striped table-hover",
+               tags$thead(tags$tr(tags$th("Measure"), tags$th("Value"))),
+               tags$tbody(
+                 tags$tr(tags$th("Skewness"), tags$td(skewness(LakeHuron))),
+                 tags$tr(tags$th("Kurtosis / Excess"), tags$td(kurtosis(LakeHuron))),
+                 tags$tr(tags$th("Variance"), tags$td(var(LakeHuron)))
+               )
+    )
+  )
+  })
+  output$locations <- renderUI({tagList(
+    tags$h4("Locations"),
+    tags$table(class="table table-condensed table-bordered table-striped table-hover",
+               tags$thead(tags$tr(tags$th("Location"), tags$th("Value"))),
+               tags$tbody(
+                 tags$tr(tags$th("Mean"), tags$td(mean(LakeHuron))),
+                 tags$tr(tags$th("Median"), tags$td(median(LakeHuron))),
+                 tags$tr(tags$th("Weighted Mean"), tags$td(weighted.mean(LakeHuron))),
+                 tags$tr(tags$th("Trimmed Mean (10%)"), tags$td(mean(LakeHuron, trim = 0.10)))
+               )
+    )
+  )
+  })
+  output$variations <- renderUI({tagList(
+    tags$h4("Variations"),
+    tags$table(class="table table-condensed table-bordered table-striped table-hover",
+               tags$thead(tags$tr(tags$th("Variation"), tags$th("Value"))),
+               tags$tbody(
+                 tags$tr(tags$th("Variance"), tags$td(var(LakeHuron))),
+                 tags$tr(tags$th("Standard Deviation"), tags$td(sd(LakeHuron))),
+                 tags$tr(tags$th("Range"), tags$td(range(LakeHuron))),
+                 tags$tr(tags$th("MAD (median)"), tags$td(mad(LakeHuron)))
+               )
+    )
+  )
   })
 }
 
